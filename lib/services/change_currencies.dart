8cad3036e91cc8226fd6f107f707ca7f47c8a143
andres <andresc.gomez@upb.edu.co>
2023-04-30 20:58:41 -0500

    Update change_currencies.dart

--- lib/services/change_currencies.dart
+++ lib/services/change_currencies.dart
@@ -2,8 +2,16 @@ import 'package:money_converter/Currency.dart';
 import 'package:http/http.dart' as http;
 
 String copToUsd(amount) {
-  var amountParsed = double.parse(amount);
-  var cop = amountParsed / 4699;
-  String result = cop.toStringAsFixed(2);
+  String result;
+  if( amount == ''){
+    var cop = 0;
+    result = cop.toStringAsFixed(2);
+  }
+  else{
+    var amountParsed = double.parse(amount);
+    var cop = amountParsed / 4699;
+    result = cop.toStringAsFixed(2);
+  }
+  
   return result;
 }
