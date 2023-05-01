151949c40bab671de4e307c807cdd40e1d4e1586
andres <andresc.gomez@upb.edu.co>
2023-04-30 21:57:49 -0500

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
               height: 15,
             ),
             TextField(
-              keyboardType: TextInputType.number,
+              keyboardType: const TextInputType.numberWithOptions(),
               decoration: const InputDecoration(
                 filled: true,
                 fillColor: Color(0xFFD9D9D9),
